7d24a527a3f81a18ef22a91974ee8f1783960021
Michał Mikołajek <mmikoajek0@gmail.com>
2022-03-31 16:06:20 +0200

    Support for floating point numbers has been implemented.

--- Calculator.py
+++ Calculator.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/evaluate', methods=['POST'])
 def calculations():
     forbidden_letters = set(string.ascii_lowercase + string.ascii_uppercase)
-    forbidden_characters = ";:,?'|!@#$%^&_][{}£§."
+    forbidden_characters = ";:,?'|!@#$%^&_][{}£§"
     request_data = request.get_json()
     error_msg = jsonify(error='validation error')
     if 'expression' in request_data:
@@ -19,12 +19,22 @@ def calculations():
         return error_msg
     elif '//' in output:
         return error_msg
+    elif '..' in output:
+        return error_msg
+    elif output.endswith('.') or output.startswith('.'):
+        return error_msg
 
-    for item in output:
+    for i, item in enumerate(output):
         if item in forbidden_letters:
             return error_msg
         elif item in forbidden_characters:
             return error_msg
+        elif item == '.':
+            try:
+                int(output[i - 1])
+                int(output[i + 1])
+            except ValueError:
+                return error_msg
 
     output = round((eval(output)))
     return jsonify(result=output)
