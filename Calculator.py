ca8a1f6ffb06ab44199ca26ad29a12ffb742255a
Michał Mikołajek <mmikoajek0@gmail.com>
2022-04-02 17:42:41 +0200

    Varibles for more complicated calculation structure and ray module have been added

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,5 @@
 import string
+import ray
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -6,38 +7,51 @@ app = Flask(__name__)
 
 @app.route('/evaluate', methods=['POST'])
 def calculations():
+    # Variables for validation
     forbidden_letters = set(string.ascii_lowercase + string.ascii_uppercase)
     forbidden_characters = ";:,?'|!@#$%^&_][{}£§"
     request_data = request.get_json()
     error_msg = jsonify(error='validation error')
     if 'expression' in request_data:
-        output = request_data['expression']
+        holder = request_data['expression']
     else:
         return error_msg
 
-    if '**' in output:
+    if '**' in holder:
         return error_msg
-    elif '//' in output:
+    elif '//' in holder:
         return error_msg
-    elif '..' in output:
+    elif '..' in holder:
         return error_msg
-    elif output.endswith('.') or output.startswith('.'):
+    elif holder.endswith('.') or holder.startswith('.'):
         return error_msg
 
-    for i, item in enumerate(output):
+    for i, item in enumerate(holder):
         if item in forbidden_letters:
             return error_msg
         elif item in forbidden_characters:
             return error_msg
         elif item == '.':
             try:
-                int(output[i - 1])
-                int(output[i + 1])
+                int(holder[i - 1])
+                int(holder[i + 1])
             except ValueError:
                 return error_msg
+    # Variables for calculations
+    brackets_tier_3 = "()"
+    mathematical_op_tier_2 = "/*"
+    mathematical_op_tier_1 = "+-"
+    pointer = int((len(aaa) - 1) / 2)
+    left_side_exp = ""
+    right_side_exp = ""
+    flag = True
+    flag_2 = True
+    holder = 0
+    x = 0
+    y = 0
 
-    output = round((eval(output)))
-    return jsonify(result=output)
+    holder = round((eval(holder)))
+    return jsonify(result=holder)
 
 
 if __name__ == '__main__':
